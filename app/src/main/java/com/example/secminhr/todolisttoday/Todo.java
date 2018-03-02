b0168619b9782c0d82078ef97e42d6c18a2f7ade
secminhr <secminhrian@gmail.com>
2018-03-02 22:34:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/secminhr/todolisttoday/Todo.java
@@ -0,0 +1,5 @@
+package com.example.secminhr.todolisttoday;
+
+public class Todo {
+    
+}
---
+++ app/src/main/java/com/example/secminhr/todolisttoday/TodoAdapter.java
@@ -0,0 +1,24 @@
+package com.example.secminhr.todolisttoday;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class TodoAdapter extends RecyclerView.Adapter {
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
